1dd456817e1d5561c76da2d29a5b5b36efd98360
Ostap Kravchuk <ostap.kravchuk@neonstingray.com>
2017-03-28 23:29:59 +0200

    Add precondition decorator file

---
+++ src/validation/precondition.ts
@@ -0,0 +1,8 @@
+/**
+ * This decorator allows execution of the method it is applied to if provided conditions are satisfied
+ */
+export function Precondition(conditions) : MethodDecorator {
+    return function decorate(target, key: string) {
+
+    }
+}
